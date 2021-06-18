64d608cee8099bdc2555c29213e7695575812e02
Garima <gammo0508@gmail.com>
2021-06-19 01:07:40 +0530

    added to localStorage

--- background.js
+++ background.js
@@ -1,6 +1,29 @@
-window.meanings = {};
+// window.meanings = {};
+// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   window.meanings[request.word] = request.meaning;
+// });
+
+// chrome.browserAction.onClicked.addListener(function (tab) {
+//   chrome.tabs.create({ url: "popup.html" });
+// });
+
+// window.meanings = {};
+
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  window.meanings[request.word] = request.meaning;
+  // if (localStorage.getItem("meanings") === null) {
+  //   localStorage.setItem("meanings", {});
+  // }
+  var meanings = JSON.parse(localStorage.getItem("meanings") || "[]");
+  // var old_data = {};
+  // if (localStorage.getItem("meanings") !== null) {
+  //   old_data = JSON.parse(localStorage.getItem("meanings"));
+  // }
+  var meaning = { word: request.word, meaning: request.meaning };
+  if (!meanings.some((key) => key.word === meaning.word)) {
+    meanings.push(meaning);
+    console.log("Added" + meaning);
+    localStorage.setItem("meanings", JSON.stringify(meanings));
+  }
 });
 
 chrome.browserAction.onClicked.addListener(function (tab) {
--- popup.js
+++ popup.js
@@ -1,10 +1,44 @@
+// document.addEventListener(
+//   "DOMContentLoaded",
+//   function () {
+//     const bg = chrome.extension.getBackgroundPage();
+//     Object.keys(bg.meanings).forEach(function (word) {
+//       const div = document.createElement("div");
+//       div.textContent = `${word} : ${bg.meanings[word]}`;
+//       // chrome.storage.sync.set({ url: bg.meanings[word] }, function () {
+//       //   //  Data's been saved boys and girls, go on home
+//       //   alert("Success");
+//       // });
+
+//       document.body.appendChild(div);
+//     });
+
+//     // document.querySelector('button').addEventListener('click', onclick, false)
+//     //
+//     // function onclick () {
+//     //   chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
+//     //     chrome.tabs.sendMessage(tabs[0].id, 'hi', setCount)
+//     //   })
+//     // }
+//     //
+//     // function setCount (res) {
+//     //   const div = document.createElement('div')
+//     //   div.textContent = `${res.count} bears`
+//     //   document.body.appendChild(div)
+//     // }
+//   },
+//   false
+// );
 document.addEventListener(
   "DOMContentLoaded",
   function () {
     const bg = chrome.extension.getBackgroundPage();
-    Object.keys(bg.meanings).forEach(function (word) {
+    var retrievedData = localStorage.getItem("meanings");
+    var arr = JSON.parse(retrievedData);
+
+    arr.forEach(function (mean) {
       const div = document.createElement("div");
-      div.textContent = `${word} : ${bg.meanings[word]}`;
+      div.textContent = `${mean.word} : ${mean.meaning}`;
       // chrome.storage.sync.set({ url: bg.meanings[word] }, function () {
       //   //  Data's been saved boys and girls, go on home
       //   alert("Success");
